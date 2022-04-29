93b54b9764e758c86de9899ce9516f276bd027a9
Alex Zero <joseph@marsden.space>
2022-04-29 21:52:35 +0100

    refactor: simplify crds module

--- modules/crds/default.nix
+++ modules/crds/default.nix
@@ -1,16 +1,5 @@
-{ config, lib, ... }: let
-    inherit (lib) kube mkIf;
-    cfg = config.crds;
+{ lib, ... }: let
+    inherit (lib) kube;
 in {
-    options.crds = with lib; {
-        enable = mkOption {
-            type = types.bool;
-            default = true;
-            description = "Enable CRDs";
-        };
-    };
-
-    config = mkIf cfg.enable {
-        resources.crds = kube.compileKustomization ./.;
-    };
+    config.resources.crds = kube.compileKustomization ./.;
 }
