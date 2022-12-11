56eb83cf7eba4d024e6d02ee3f011fd95eac8600
David Arnold <david.arnold@iohk.io>
2022-12-11 12:10:57 -0500

    refactor: standardize fractal I/II
    the motivation for standardizing fractal is to reduce boilerplate
    increase mindshare and lower the effort to contribute for std users.
    
    std users will likely become very active users of and contributors
    to fractal in the near future.

--- flake.nix
+++ flake.nix
@@ -1,49 +1,35 @@
 {
-    description = "Fractal Kubernetes Framework";
-    inputs = rec {
-        xnlib.url = "github:ArctarusLimited/xnlib";
-        flake-utils.url = "github:numtide/flake-utils";
+  description = "Fractal Kubernetes Framework";
+  inputs.std.url = "github:divnix/std";
+  inputs.std.inputs.nixpkgs.follows = "nixpkgs";
+  inputs.std.inputs.mdbook-kroki-preprocessor.follows = "std/blank";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  inputs = {
+    xnlib.url = "github:ArctarusLimited/xnlib";
+  };
+
+  outputs = {
+    self,
+    std,
+    ...
+  } @ inputs:
+    std.growOn {
+      inherit inputs;
+      cellsFrom = std.incl ./. [
+        ./lib # library
+        ./app # fractal cli
+      ];
+      cellBlocks = with std.blockTypes; [
+        # library
+        (functions "builders")
+        (functions "generators")
+        (functions "validators")
+        # fractal cli
+        (installables "packages")
+      ];
+    }
+    {
+      packages = std.harvest self ["app" "packages"];
+      lib = import ./lib {inherit inputs;};
     };
-
-    outputs = { self, nixpkgs, flake-utils, ... }@inputs: let
-        inherit (flake-utils.lib) eachDefaultSystem flattenTree;
-    in {
-        lib = import ./lib { inherit inputs; };
-    } // (eachDefaultSystem (system: let
-        pkgs = nixpkgs.legacyPackages.${system};
-    in rec {
-        packages = flattenTree {
-            fractal = let
-                inherit (pkgs) lib buildGoModule makeWrapper
-                    kubernetes-helm kustomize;
-            in pkgs.buildGoModule rec {
-                pname = "fractal";
-                version = "1.0.0";
-                src = ./app;
-
-                buildInputs = [ makeWrapper ];
-                vendorSha256 = "sha256-BIqJ1PRJjIy/Z7ILr4mA6dE9IqBQabQt/YHgSgajRhw=";
-
-                postInstall = ''
-                    mv "$out/bin/app" "$out/bin/fractal"
-                '';
-
-                postFixup = let
-                    runtimeDeps = [ kubernetes-helm kustomize ];
-                in ''
-                    wrapProgram "$out/bin/fractal" \
-                        --prefix PATH ":" ${lib.makeBinPath runtimeDeps}
-                '';
-
-                meta = with lib; {
-                    description = "Nix-based framework for building Kubernetes resources";
-                    homepage = "https://github.com/ArctarusLimited/Fractal";
-                    license = licenses.mit;
-                    maintainers = [ maintainers.citadelcore ];
-                };
-            };
-        };
-
-        defaultPackage = packages.fractal;
-    }));
 }
